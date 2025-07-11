6c1cbe2eefdd4668b93cdcbee52797ef8af1f4d4
abananaj <anna@theatrum.dev>
2025-07-10 17:11:46 -0700

    add new homepg

---
+++ dev/content/home-nav-container/single-pg-nav.js
@@ -0,0 +1,84 @@
+class StickyNavigation {
+	constructor() {
+		this.currentId = null;
+		this.currentTab = null;
+		this.tabContainerHeight = 70;
+		let self = this;
+		$(".nav-tab a").click(function () {
+			self.onTabClick(event, $(this));
+		});
+		$(window).scroll(() => {
+			this.onScroll();
+		});
+		$(window).resize(() => {
+			this.onResize();
+		});
+	}
+
+	onTabClick(event, element) {
+		event.preventDefault();
+		let scrollTop =
+			$(element.attr("href")).offset().top - this.tabContainerHeight + 1;
+		$("html, body").animate({ scrollTop: scrollTop }, 600);
+	}
+
+	onScroll() {
+		this.checkTabContainerPosition();
+		this.findCurrentTabSelector();
+	}
+
+	onResize() {
+		if (this.currentId) {
+			this.setSliderCss();
+		}
+	}
+
+	checkTabContainerPosition() {
+		let offset =
+			$(".pg-header").offset().top +
+			$(".pg-header").height() -
+			this.tabContainerHeight;
+		if ($(window).scrollTop() > offset) {
+			$(".pg-menu").addClass("pg-menu--top");
+		} else {
+			$(".pg-menu").removeClass("pg-menu--top");
+		}
+	}
+
+	findCurrentTabSelector(element) {
+		let newCurrentId;
+		let newCurrentTab;
+		let self = this;
+		$(".nav-tab a").each(function () {
+			let id = $(this).attr("href");
+			let offsetTop = $(id).offset().top - self.tabContainerHeight;
+			let offsetBottom =
+				$(id).offset().top + $(id).height() - self.tabContainerHeight;
+			if (
+				$(window).scrollTop() > offsetTop &&
+				$(window).scrollTop() < offsetBottom
+			) {
+				newCurrentId = id;
+				newCurrentTab = $(this);
+			}
+		});
+		if (this.currentId != newCurrentId || this.currentId === null) {
+			this.currentId = newCurrentId;
+			this.currentTab = newCurrentTab;
+			this.setSliderCss();
+		}
+	}
+
+	setSliderCss() {
+		let width = 0;
+		let left = 0;
+		if (this.currentTab) {
+			width = this.currentTab.css("width");
+			left = this.currentTab.offset().left;
+		}
+		$(".nav-tab-indicator").css("width", width);
+		$(".nav-tab-indicator").css("left", left);
+	}
+}
+
+new StickyNavigation();
